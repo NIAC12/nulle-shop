7e0dd7db987d50d5bcbbdd7894f9a196a1ca4fa8
NIAC12 <se6583@gmail.com>
2024-07-21 21:02:41 +0900

    Update and rename server.js to checkout.js

---
+++ checkout.js
@@ -0,0 +1,14 @@
+document.getElementById('checkout-form').onsubmit = function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const address = document.getElementById('address').value;
+    const card = document.getElementById('card').value;
+
+    if (name && address && card) {
+        alert('결제가 완료되었습니다. 감사합니다!');
+        localStorage.removeItem('cart');
+        window.location.href = 'index.html';
+    } else {
+        alert('모든 필드를 채워주세요.');
+    }
+}
