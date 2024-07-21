0d3856d53f75b32677e43661d22196341811ad07
NIAC12 <se6583@gmail.com>
2024-07-21 21:20:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // 제품 목록 데이터 (이미지 파일 경로를 업데이트)
 const products = [
-    { id: 1, name: '눌레 키링', price: 10000, description: '귀여운 눌레 키링입니다.', image: 'https://github.com/NIAC12/nulle-store/blob/main/images/keyring.jpg?raw=true' },
+    { id: 1, name: '눌레 키링', price: 8000, description: '귀여운 눌레 키링입니다.', image: 'https://github.com/NIAC12/nulle-store/blob/main/images/keyring.jpg?raw=true' },
     { id: 2, name: '눌레 스마트폰 홀더', price: 15000, description: '편리한 눌레 스마트폰 홀더입니다.', image: 'https://github.com/NIAC12/nulle-store/blob/main/images/SPholder.jpg?raw=true' },
-    { id: 3, name: '눌레 티셔츠', price: 20000, description: '편안한 눌레 티셔츠입니다.', image: 'https://github.com/NIAC12/nulle-store/blob/main/images/Tshirts.jpg?raw=true' },
+    { id: 3, name: '눌레 티셔츠', price: 25000, description: '편안한 눌레 티셔츠입니다.', image: 'https://github.com/NIAC12/nulle-store/blob/main/images/Tshirts.jpg?raw=true' },
 ];
 
 window.onload = function() {
@@ -32,7 +32,7 @@ window.onload = function() {
             const newProduct = {
                 id: products.length + 1,
                 name: name,
-                price: price,
+                price: parseInt(price, 10),
                 description: description,
                 image: e.target.result
             };
@@ -49,13 +49,14 @@ function displayProducts(products) {
     const productList = document.getElementById('product-list');
     productList.innerHTML = '';
     products.forEach(product => {
+        const formattedPrice = product.price.toLocaleString('ko-KR') + ' ₩';
         const productElement = document.createElement('div');
         productElement.classList.add('bg-white', 'p-4', 'shadow-md', 'rounded-lg', 'text-center');
         productElement.innerHTML = `
             <img src="${product.image}" alt="${product.name}" class="w-full h-64 object-cover mb-4 rounded-lg">
             <h2 class="text-xl font-bold mb-2">${product.name}</h2>
             <p class="text-gray-600 mb-2">${product.description}</p>
-            <p class="text-gray-800 font-semibold mb-4">${product.price}원</p>
+            <p class="text-gray-800 font-semibold mb-4">${formattedPrice}</p>
             <button onclick="addToCart(${product.id})" class="bg-gray-800 text-white px-4 py-2 rounded">장바구니에 추가</button>
         `;
         productList.appendChild(productElement);
